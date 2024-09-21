7eef3dbe45de567080546f6b0a6f76bfb2016bea
Ireme-Nyawe <akimana.inono@gmail.com>
2024-09-21 16:29:27 +0200

    array move extras

---
+++ test2.py
@@ -0,0 +1,15 @@
+arr1=[1,2,3,4]
+
+arr1[1]=10
+arr1.append(20)
+arr1.insert(3,5)
+arr1.extend([12,4,12,13])
+arr1.remove(12)
+arr1.pop()
+del arr1[2]
+
+print(arr1)
+
+arr2d=[[1,2,[2,3]],[6,3,]]
+arr2d.pop(0)
+print(arr2d)
